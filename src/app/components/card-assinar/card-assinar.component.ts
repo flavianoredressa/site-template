52830d2340f6efde0054c2e856aa6dd7d66628ca
Laura Faquinha <111758190+LFaquiinha@users.noreply.github.com>
2023-12-11 12:08:47 -0300

    feat (card-assinar): add componente card assinar

---
+++ src/app/components/card-assinar/card-assinar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-assinar',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-assinar.component.html',
+  styleUrl: './card-assinar.component.scss'
+})
+export class CardAssinarComponent {
+
+}
